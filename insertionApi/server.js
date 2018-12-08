7b6833cef4d2d2234ed1d1227971e772e02265f8
dabukofyuslesnoleyj <chinomanga@outlook.com>
2018-12-08 23:07:31 +0800

    changed to fit online deployment

--- insertionApi/server.js
+++ insertionApi/server.js
@@ -1,3 +1,6 @@
+/* global process */
+var port = process.env.PORT || 8000;
+
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -12,28 +15,14 @@ app.use(express.json());
 
 app.post('/insertionSort2', function (req, res) {
     var data = req.body.elements
-    var sorted_data = insertionSort(data);
+    var sorted_data = data.sort();
     res.send(sorted_data)
     res.end();
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
     console.log(host)
-    console.log("Example app listening at http://%s:%s", host, port)
-})
-
-let insertionSort = (inputArr) => {
-    let length = inputArr.length;
-    for (let i = 1; i < length; i++) {
-        let key = inputArr[i];
-        let j = i - 1;
-        while (j >= 0 && inputArr[j] > key) {
-            inputArr[j + 1] = inputArr[j];
-            j = j - 1;
-        }
-        inputArr[j + 1] = key;
-    }
-    return inputArr;
-};
+    console.log("App listening at http://%s:%s", host, port)
+});
